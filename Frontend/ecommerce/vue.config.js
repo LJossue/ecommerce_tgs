cd278da0c128e7843f52e4879d38a40bc3f1f99c
Leandro Jossue Ramirez Vezga <101231796+LJossue@users.noreply.github.com>
2024-06-06 10:36:30 -0500

    Delete Frontend directory

--- Frontend/ecommerce/vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
-module.exports = {
-  devServer: {
-    port: 3000, // Puedes especificar el puerto que desees aquí
-  },
-};
